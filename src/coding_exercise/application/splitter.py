88a4f14d7f9df2d589347c94b6c9f0c62cb66020
Prabhat kumar singh <prabhat111s@gmail.com>
2024-12-15 13:39:31 +1100

    adding comments and finalising changes

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -5,6 +5,9 @@ class Splitter:
 
     def __validate(self, len_cable:int, times:int) -> bool:
         """
+        Validating based on
+        1. Min Max constraints of cable and times  
+        2. Number of split times cannot be more than cable length
         """
         # constraints check
         if len_cable >=2 and len_cable <=1024 and times >=1 and times <=64:
@@ -16,24 +19,30 @@ class Splitter:
         else:
             raise ValueError
 
+
     def _set_cable_name(self, index:int, cable_name:str) -> str:
+        """
+        Cable name's should be of the form "name-00", "name-01" .. "name-10", "name-11"..
+        """
         if index > 9:
             return cable_name + "-" + str(index)
         else:
             return cable_name + "-0" + str(index)
 
+
     def split(self, cable: Cable, times: int) -> list[Cable]:
         self.__validate(cable.length, times)
         max_split_len = cable.length//(times+1)
-
         remaining_cable = cable.length
         results = []
         i=0
         while remaining_cable > 0:
             if remaining_cable >= max_split_len:
+                # adding max length split cable
                 results.append(Cable(length=max_split_len, name=self._set_cable_name(i, cable.name)))
                 remaining_cable -= max_split_len
             else:
+                # adding the remaining cable and breaking the loop
                 results.append(Cable(length=remaining_cable, name=self._set_cable_name(i, cable.name)))
                 remaining_cable = 0
                 break
--- tests/coding_exercise/application/test_validate_splits.py
+++ tests/coding_exercise/application/test_validate_splits.py
@@ -3,6 +3,9 @@ from coding_exercise.domain.model.cable import Cable
 import pytest
 
 def test_validate_split_len():
+    """
+    Validating the splits length
+    """
     cables = Splitter().split(Cable(10, "coconuts"), 2)
     valid_split = [3,3,3,1]
     assert len(cables) == len(valid_split)
@@ -20,13 +23,20 @@ def test_validate_split_len():
 
 
 def test_validate_split_names():
+    """
+    Validating the new splits name
+    """
     cables = Splitter().split(Cable(10, "coconuts"), 1)
-    valid_names = ["coconuts-00", "coconuts-01"]
-    assert len(cables) == len(valid_names)
-    assert [cable.name for cable in cables] == valid_names
+    assert [cable.name for cable in cables] == ["coconuts-0"+str(i) for i in range(2)]
+
+    cables = Splitter().split(Cable(11, "coconuts"), 3)
+    assert [cable.name for cable in cables] == ["coconuts-0"+str(i) for i in range(6)]
 
 
 def test_validate_constraints():
+    """
+    Validating the constraints which will throw ValueError
+    """
     with pytest.raises(ValueError):
         cables = Splitter().split(Cable(100, "coconuts"), 65)
 
